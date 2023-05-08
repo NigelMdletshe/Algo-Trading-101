38fc0caa642d49dcdd4bce9cc85d745bef37ef7f
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-08 21:20:32 +0200

    code complete but takes forever to load due to network/ error

--- AlgoTrading.py
+++ AlgoTrading.py
@@ -21,13 +21,29 @@ dates = pd.date_range(start,end, freq= "1H")
 dates = [ int(x.value/10**9) for x in list (dates)]
 #print(dates)
 
+master_data= []
+
 for first, last in zip(dates,dates[1:]):
+    #print(first,last)
     params = {
         "step":60,
         "limit":1000,
+        "start":first,
+        "end":last,
     }
 
     data = requests.get(url,params=params)
     data = data.json()["data"]["ohlc"]
-    df = pd.DataFrame(data)
-    print(df)
+    master_data += data
+
+
+df = pd.DataFrame(master_data)
+df = df.drop_duplicates()
+
+df["timestamp"] = df["timestamp"]
+df = df.sort_values(by="timestamp")
+df = df[df["timestamp"] >= dates[0]]
+df = df[df["timestamp"] < dates[-1]]
+print(df)
+df.to_csv("bitcoinData.csv", index = False)
+
