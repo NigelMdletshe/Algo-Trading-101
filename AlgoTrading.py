de94d43f57c016130665b83b963b9b80f3636203
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-08 21:38:48 +0200

    #there is an error with the code or it is caused by the network, see last comment

--- AlgoTrading.py
+++ AlgoTrading.py
@@ -11,39 +11,52 @@ import pandas as pd
 import requests
 import datetime
 
+
+
+# Set the currency pair and API endpoint
 currency_pair = "btcusd"
 url = f"https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/"
 
+# Define the date range
 start = "2020-01-01"
-end   = "2023-05-08"
+end = "2023-05-08"
 
-dates = pd.date_range(start,end, freq= "1H")
-dates = [ int(x.value/10**9) for x in list (dates)]
-#print(dates)
+# Generate a list of hourly timestamps within the date range
+dates = pd.date_range(start, end, freq="1H").tolist()
+timestamps = [int(date.timestamp()) for date in dates]
 
-master_data= []
+# Fetch data for each timestamp interval
+master_data = []
 
-for first, last in zip(dates,dates[1:]):
-    #print(first,last)
+for i in range(len(timestamps) - 1):
+    first = timestamps[i]
+    last = timestamps[i + 1]
+
     params = {
-        "step":60,
-        "limit":1000,
-        "start":first,
-        "end":last,
+        "step": 60,
+        "limit": 1000,
+        "start": first,
+        "end": last,
     }
 
-    data = requests.get(url,params=params)
-    data = data.json()["data"]["ohlc"]
-    master_data += data
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()["data"]["ohlc"]
+        master_data += data
 
-
+# Create a DataFrame from the collected data
 df = pd.DataFrame(master_data)
-df = df.drop_duplicates()
 
-df["timestamp"] = df["timestamp"]
+# Clean and filter the DataFrame
+df = df.drop_duplicates()
+df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s")
 df = df.sort_values(by="timestamp")
-df = df[df["timestamp"] >= dates[0]]
-df = df[df["timestamp"] < dates[-1]]
+df = df[(df["timestamp"] >= start) & (df["timestamp"] <= end)]
+
+# Save the DataFrame to a CSV file
+df.to_csv("bitcoinData.csv", index=False)
+
+# Print the resulting DataFrame
 print(df)
-df.to_csv("bitcoinData.csv", index = False)
+# This code takes a long time to print and ssave data, not sure what is the error, l am suspecting the network
 
