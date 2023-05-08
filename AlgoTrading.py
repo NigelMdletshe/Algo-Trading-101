52af6ee71326da686e02fa5bb937dceba8163635
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-08 20:40:59 +0200

    Scrapped Data from Bitstamp

---
+++ AlgoTrading.py
@@ -0,0 +1,23 @@
+#1 Data Scraping
+#2 Backtesting
+#3 Execution Code
+#4 Deploy bot to cloud
+#5 Trade Analysis
+
+#Scrap data from Bitstamp
+
+import json
+import pandas as pd
+import requests
+import datetime
+
+currency_pair = "btcusd"
+url = f"https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/"
+
+params = {
+    "step":60,
+    "limit":1000,
+}
+
+data = requests.get(url,params=params)
+print(data.json())
