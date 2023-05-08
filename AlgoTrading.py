5689a41f35afe343f971c81424b52cd7877ce04f
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-08 20:54:58 +0200

    added a for loop to make multiple request

--- AlgoTrading.py
+++ AlgoTrading.py
@@ -14,12 +14,20 @@ import datetime
 currency_pair = "btcusd"
 url = f"https://www.bitstamp.net/api/v2/ohlc/{currency_pair}/"
 
-params = {
-    "step":60,
-    "limit":1000,
-}
+start = "2020-01-01"
+end   = "2023-05-08"
 
-data = requests.get(url,params=params)
-data = data.json()["data"]["ohlc"]
-df = pd.DataFrame(data)
-print(df)
+dates = pd.date_range(start,end, freq= "1H")
+dates = [ int(x.value/10**9) for x in list (dates)]
+#print(dates)
+
+for first, last in zip(dates,dates[1:]):
+    params = {
+        "step":60,
+        "limit":1000,
+    }
+
+    data = requests.get(url,params=params)
+    data = data.json()["data"]["ohlc"]
+    df = pd.DataFrame(data)
+    print(df)
